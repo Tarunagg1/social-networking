9dda21b99ca222a001617e344b1f3dfdff408a6a
Tarun <arunaggarwal096@gmail.com>
2020-06-08 18:37:58 +0530

    final

--- css/leargit.php
+++ css/leargit.php
@@ -6,13 +6,20 @@ git config --globle user.email your email
 git config --globle user.name
 
 <!-- .start here -->
-
 git init
 ls -lart
 git status  //to know the status of file tracked or untrackrd
 git add     //use to add a single file in track mode
 git add -A     //use to add a all file in track mode
 git commit  //for final commit open vim to enter a msgfmt_format_message
+git commit -m "Your message"  //for final commit but not open vim editor
+
+<!-- Operational in git -->
+git checkout filename    ////for recover one file in commit
+git checkout -f      ////for recover all file in commit
+git log    //// to show all commit and time
+git log -p -no of commit  //// to show filter commit log
 
 
+<!-- git dif -->
 
